d1c32d41215809889133d282472eda5f0c985471
alhammad108 <115951421+alhammad108@users.noreply.github.com>
2022-11-28 20:16:05 +0300

    Update Check_High_Price.java

--- src/Check_High_Price.java
+++ src/Check_High_Price.java
@@ -13,7 +13,7 @@ import com.github.dockerjava.core.dockerfile.DockerfileStatement.Add;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class Check_High_Price {
+public class check_High_Price {
 
 	public WebDriver driver;
 
@@ -23,32 +23,32 @@ public class Check_High_Price {
 		WebDriverManager.chromedriver().setup();
 
 		driver = new ChromeDriver();
-		String User_Name = "standard_user";
+		String user_Name = "standard_user";
 		String Password = "secret_sauce";
 
 		driver.get("https://www.saucedemo.com/");
 
-		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys(User_Name);
+		driver.findElement(By.xpath("//*[@id=\"user-name\"]")).sendKeys(user_Name);
 		Thread.sleep(2000);
 
 		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys(Password);
 		driver.findElement(By.xpath("//*[@id=\"login-button\"]")).click();
 	}
 @Test()
-public void High_Price() throws InterruptedException {
+public void high_Price() throws InterruptedException {
 	driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select")).click();
 	Thread.sleep(2000);
 	driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select/option[4]")).click();
-List<WebElement>  High_Price_List=driver.findElements(By.className("inventory_item_price"));
+List<WebElement>  high_Price_List=driver.findElements(By.className("inventory_item_price"));
 List<Double> Addtion=new ArrayList<>();
 for(int i=0;i<High_Price_List.size();i++) {
-	String Hieh_Price=High_Price_List.get(i).getText();
+	String hieh_Price=high_Price_list.get(i).getText();
 	
-	String Update_High_Price =Hieh_Price.replace("$", "");
+	String update_High_Price =hieh_Price.replace("$", "");
 	
-	Hieh_Price.trim();
+	hieh_Price.trim();
 	
-	  double val = Double.parseDouble(Update_High_Price);
+	  double val = Double.parseDouble(update_High_Price);
 	  Addtion.add(val);
 	  
 	  
@@ -56,9 +56,9 @@ for(int i=0;i<High_Price_List.size();i++) {
 }
 
 for(int j=0;j<Addtion.size();j++) {
-	boolean Chech_High_Price=Addtion.get(0)>Addtion.get(Addtion.size()-1);
+	boolean chech_High_price=Addtion.get(0)>Addtion.get(Addtion.size()-1);
 	 	
-	Assert.assertEquals(Chech_High_Price , true);
+	Assert.assertEquals(chech_High_price , true);
 }
 }
 
